aaff8fddcd82f3c5be0d0607a61367146ca2fa3f
SimoPrG <simeon_petev@hotmail.com>
2015-08-25 20:45:35 +0300

    Solved task-4

--- UnderscoreOverview/tasks/task-4.js
+++ UnderscoreOverview/tasks/task-4.js
@@ -1,33 +1,78 @@
 /* 
-Create a function that:
-*   **Takes** an array of animals
-    *   Each animal has propeties `name`, `species` and `legsCount`
-*   **groups** the animals by `species`
-    *   the groups are sorted by `species` descending
-*   **sorts** them ascending by `legsCount`
-	*	if two animals have the same number of legs sort them by name
-*   **prints** them to the console in the format:
+ Create a function that:
+ *   **Takes** an array of animals
+ *   Each animal has propeties `name`, `species` and `legsCount`
+ *   **groups** the animals by `species`
+ *   the groups are sorted by `species` descending
+ *   **sorts** them ascending by `legsCount`
+ *	if two animals have the same number of legs sort them by name
+ *   **prints** them to the console in the format:
 
-```
-    ----------- (number of dashes is equal to the length of the GROUP_1_NAME + 1)
-    GROUP_1_NAME:
-    ----------- (number of dashes is equal to the length of the GROUP_1_NAME + 1)
-    NAME has LEGS_COUNT legs //for the first animal in group 1
-    NAME has LEGS_COUNT legs //for the second animal in group 1
-    ----------- (number of dashes is equal to the length of the GROUP_2_NAME + 1)
-    GROUP_2_NAME:
-    ----------- (number of dashes is equal to the length of the GROUP_2_NAME + 1)
-    NAME has LEGS_COUNT legs //for the first animal in the group 2
-    NAME has LEGS_COUNT legs //for the second animal in the group 2
-    NAME has LEGS_COUNT legs //for the third animal in the group 2
-    NAME has LEGS_COUNT legs //for the fourth animal in the group 2
-```
-*   **Use underscore.js for all operations**
-*/
+ ```
+ ----------- (number of dashes is equal to the length of the GROUP_1_NAME + 1)
+ GROUP_1_NAME:
+ ----------- (number of dashes is equal to the length of the GROUP_1_NAME + 1)
+ NAME has LEGS_COUNT legs //for the first animal in group 1
+ NAME has LEGS_COUNT legs //for the second animal in group 1
+ ----------- (number of dashes is equal to the length of the GROUP_2_NAME + 1)
+ GROUP_2_NAME:
+ ----------- (number of dashes is equal to the length of the GROUP_2_NAME + 1)
+ NAME has LEGS_COUNT legs //for the first animal in the group 2
+ NAME has LEGS_COUNT legs //for the second animal in the group 2
+ NAME has LEGS_COUNT legs //for the third animal in the group 2
+ NAME has LEGS_COUNT legs //for the fourth animal in the group 2
+ ```
+ *   **Use underscore.js for all operations**
+ */
 
-function solve(){
-  return function (animals) {
-  };
+function solve() {
+    'use strict';
+
+    return function (animals) {
+        var groupedAnimals = _.groupBy(animals, function (item) {
+            return item.species;
+        });
+
+        var sortedGroupedAnimalsKeys = _.sortBy(_.keys(groupedAnimals), function (item) {
+            return item;
+        })
+            .reverse();
+
+        var sortedGroupedAnimalsValues = _.chain(sortedGroupedAnimalsKeys)
+            .map(function (item) {
+                return groupedAnimals[item];
+            })
+            .each(function (item, key, array) {
+                array[key] = _.chain(item)
+                    .sortBy(function (item) {
+                        return item.name;
+                    })
+                    .sortBy(function (item) {
+                        return item.legsCount;
+                    })
+                    .value();
+            })
+            .value();
+
+        _.each(sortedGroupedAnimalsKeys, function (item, index) {
+            console.log(_.map(new Array(item.length + 1), function () {
+                return '-';
+            })
+                .join(''));
+
+            console.log(item + ':');
+
+            console.log(_.map(new Array(item.length + 1), function () {
+                return '-';
+            })
+                .join(''));
+
+            var currentGroup = sortedGroupedAnimalsValues[index];
+            _.each(currentGroup, function (item) {
+                console.log(item.name + ' has ' + item.legsCount + ' legs');
+            })
+        });
+    };
 }
 
 module.exports = solve;
